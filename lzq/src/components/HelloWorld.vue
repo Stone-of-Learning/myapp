b33470eb283a09e230df58732b10b5bad9d5a28a
Snn <15135068581@163.com>
2022-08-15 09:47:07 +0800

    qwe

---
+++ lzq/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script>
+import { reactive } from 'vue';
+export default {
+  name: 'App',
+  setup() {
+    let msg = reactive(0);
+    function c() {
+      console.log(msg);
+      msg++;
+    }
+    return {
+      msg,
+      c,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button @click="c">button</button>
+  </div>
+</template>
